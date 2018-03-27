020c69e30ac0c792152dfe28f574239f9bbac1cc
Martin Novák <martin.novak@Martins-Mac-Pro.local>
2018-03-26 21:22:47 -0500

    working on node generation

--- network-graph.js
+++ network-graph.js
@@ -3,31 +3,18 @@
 // composePipe :: [(a -> b)] -> (a -> b)
 const pipe = (...fns) => fns.reduceRight ((f, g) => (...args) => f (g (...args)));
 
-const networkGraphDefaults = {
-    idealNumNodes: 70, // 1 - 300, default 70
-    maxExtraEdges: 20, // 0 - 1000, default 20
-    radiiWeightPower: 0.5, // 0.0 mesh, 0.5 balanced, 1.0 Hub-and-Spoke, default 0.5
-    driftSpeed: 1, // 0 - 100, default 1
-    repulsionForce: 1 // 0 - 100, default 1
-};
+let idealNumNodes = 10; // 1 - 300, default 70
+let maxExtraEdges = 20; // 0 - 1000, default 20
+let radiiWeightPower = 0.5; // 0.0 mesh, 0.5 balanced, 1.0 Hub-and-Spoke, default 0.5
+let driftSpeed = 1; // 0 - 100, default 1
+let repulsionForce = 1; // 0 - 100, default 1
 
-const networkGraph = {
-    idealNumNodes: 70, // 1 - 300, default 70
-    getIdealNumNodes: () => network.idealNumNodes || 70,
-    getMaxExtraEdges: () => network.maxExtraEdges || 20,
-    getRadiiWeightPower: () => network.radiiWeightPower || 0.5,
-    getDriftSpeed: () => network.driftSpeed || 1,
-    getRepulsionForce: () => network.repulsionForce || 1,
-    setIdealNumNodes: n => {
-        network.idealNumNodes = parseInt(this.value, 10);
-		network.maxExtraEdges = Math.round(parseFloat(extraEdgesElem.value) / 100 * idealNumNodes);
-    },
-    setMaxExtraEdges: number => Math.round(parseFloat(number) / 100 * getIdealNumNodes),
-    BORDER_FADE: -0.02,
-    FADE_IN_RATE: 0.06,  // In the range (0.0, 1.0]
-    FADE_OUT_RATE: 0.03,  // In the range (0.0, 1.0]
-    FRAME_INTERVAL: 20  // In milliseconds
-};
+const getNumberOfEdges = () => Math.round(maxExtraEdges / 100 * idealNumNodes);
+
+const BORDER_FADE = -0.02;
+const FADE_IN_RATE = 0.06;  // In the range (0.0, 1.0]
+const FADE_OUT_RATE = 0.03;  // In the range (0.0, 1.0]
+const FRAME_INTERVAL = 2000;  // In milliseconds, default 20
 
 // making functional and curring
 // getBoundingClientRect :: a -> b
@@ -57,18 +44,57 @@ const setSvgAttributes = dimensions => pipe (
     ))
 );
 
-// setupSvg :: a -> b
-const setupSvg = svgElement => setSvgAttributes (relDimensionsFromBounding (getBoundingClientRect (svgElement))) (svgElement);
-
 const initialize = svgElement => {
-	/*const boundRect = svgElement.getBoundingClientRect();
-	const relWidth  = boundRect.width  / Math.max(boundRect.width, boundRect.height);
-    const relHeight = boundRect.height / Math.max(boundRect.width, boundRect.height);
-    svgElem.setAttribute ('viewBox', "0 0 " + relWidth + " " + relHeight);
-    svgElem.setAttribute ('viewBox', `0 0 ${relWidth} ${relHeight}`);
-	svgElem.querySelector("rect").setAttribute("x", (relWidth  - 1) / 2);
-    svgElem.querySelector("rect").setAttribute("y", (relHeight - 1) / 2);*/
-    setupSvg (svgElement);
+    const dimensions = relDimensionsFromBounding (getBoundingClientRect (svgElement));
+    setSvgAttributes (dimensions) (svgElement);
+    const nodes = createNewNodes (dimensions) (0);
+    //stepFrame ([]) ([]) (svgElement);
+    return svgElement;
+};
+
+const updateNodes = dimensions => nodes => (nodes.map (
+    (node, index) => ({
+        // Move based on velocity
+        posX: node.posX + node.velX * driftSpeed,
+        posY: node.posY + node.velY * driftSpeed,
+        // Randomly perturb velocity, with damping
+		velX: node.velX * 0.99 + (Math.random() - 0.5) * 0.3,
+        velY: node.velY * 0.99 + (Math.random() - 0.5) * 0.3,
+        // Fade out nodes near the borders of the space or exceeding the target number of nodes
+        opacity: (index >= idealNumNodes || node.posX < BORDER_FADE || dimensions.width - node.posX < BORDER_FADE
+            || node.posY < BORDER_FADE || dimensions.height - node.posY < BORDER_FADE) ? 
+            Math.max(node.opacity - FADE_OUT_RATE, 0) :
+            Math.min(node.opacity + FADE_IN_RATE, 1)
+    })).map ((node, index) => (node.opacity === 0) ? delete nodes [index] : node) // TODO: this doesnt account for lowering ideal number of nodes
+);
+
+const createNewNode = dimensions => ({ 
+    // Random position and radius, other properties initially zero
+    posX: Math.random() * dimensions.width,
+    posY: Math.random() * dimensions.height,
+    radius: (Math.pow(Math.random(), 5) + 0.35) * 0.015,  // Skew toward smaller values
+    velX: 0.0,
+    velY: 0.0,
+    opacity: 0.0,
+});
+
+const createNewNodes = dimensions => numberOfNodes => {
+    const newNodes = [];
+    for (var i = numberOfNodes; i < idealNumNodes; i++) {
+		newNodes.push(createNewNode (dimensions));
+    }
+    return newNodes;
+};
+
+const stepFrame = nodes => edges => svgElement => {
+    const dimensions = relDimensionsFromBounding (getBoundingClientRect (svgElement))
+    const updatedNodes = updateNodes (dimensions) (nodes);
+    const newNodes = (updatedNodes.length < idealNumNodes) ? createNewNodes (dimensions) (updatedNodes.length) : [];
+    const totalNodes = [].concat (updatedNodes, newNodes);
+    console.log (totalNodes);
+    // TODO: doForceField
+    const totalEdges = [];
+    //setTimeout (() => stepFrame (totalNodes) (totalEdges) (svgElement), FRAME_INTERVAL);
 };
 
 initialize (document.querySelector("article svg"));
